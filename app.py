b399f7ec4e5e854d1e6537c4bbe0f08f6e72fb0d
TomiTsuma <tommytsuma7@gmail.com>
2023-08-26 09:47:40 +0000

    latest changes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from app_container.controllers.metric import metric_controller
 import os
 app = Flask(__name__)
+CORS(app)
 
 
 app.register_blueprint(metric_controller, url_prefix='/')
